6b25bbc393ccf135f62717693d383cca801e6cdd
Peter Sepstrup <peter@sepstrupnet.dk>
2017-09-11 09:48:47 +0200

    Refactored table manipulation to be a part of EasyTable and not VC No longer a requirement to extend EasyTablesViewController

--- EasyTables/EasyTableViewController.swift
+++ EasyTables/EasyTableViewController.swift
@@ -10,58 +10,23 @@ import UIKit
 
 open class EasyTableViewController: UITableViewController {
 
-    public var sections = Sections()
+    public var table = EasyTable()
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        table = EasyTable(tableView: self.tableView)
+    }
     
     override open func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
+        return table.count
     }
     
     override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections.at(index: section).count
+        return table.at(index: section).count
     }
     
     override open func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections.at(index: section).title
+        return table.at(index: section).title
     }
     
-    public func addRowsBelow(indexPath: IndexPath, data: [TableCellInfo]) {
-        let section = sections.at(index: indexPath.section)
-        section.rows.insert(contentsOf: data, at: indexPath.row+1)
-        var newRows: [IndexPath] = []
-        var newCount = 1
-        for _ in data {
-            newRows.append(IndexPath(row: indexPath.row+newCount, section: indexPath.section))
-            newCount = newCount+1
-        }
-        tableView.beginUpdates()
-        tableView.insertRows(at: newRows, with: .fade)
-        tableView.endUpdates()
-    }
-    
-    public func removeRowsBelow(indexPath: IndexPath, numberOfRows: Int) {
-        var delRows: [IndexPath] = []
-        let section = sections.at(index: indexPath.section)
-        for i in 1...numberOfRows {
-            section.rows.remove(at: indexPath.row+1)
-            delRows.append(IndexPath(row: indexPath.row+i, section: indexPath.section))
-        }
-        tableView.beginUpdates()
-        tableView.deleteRows(at: delRows, with: .fade)
-        tableView.endUpdates()
-    }
-    
-    public func removeRowsAbove(indexPath: IndexPath, numberOfRows: Int, includeSender: Bool = false) {
-        var delRows: [IndexPath] = []
-        let section = sections.at(index: indexPath.section)
-        let delSender: Int = includeSender ? 0 : 1
-        for i in delSender...(numberOfRows-1) {
-            section.rows.remove(at: indexPath.row-i)
-            delRows.append(IndexPath(row: indexPath.row-i, section: indexPath.section))
-        }
-        tableView.beginUpdates()
-        tableView.deleteRows(at: delRows, with: .fade)
-        tableView.endUpdates()
-        
-    }
-
 }
