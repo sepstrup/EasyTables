13b88e2c89ffb82c848e461fb1e3bcdf3a18773d
Peter Sepstrup <peter@sepstrupnet.dk>
2017-09-18 20:22:30 +0200

    index(of: TableCellInfo) added

--- EasyTables/TableSection.swift
+++ EasyTables/TableSection.swift
@@ -119,4 +119,8 @@ public class TableSection: Equatable {
         return rows[at]
     }
     
+    public func index(of: TableCellInfo) -> Int? {
+        return rows.index(of: of)
+    }
+    
 }
